e0a8f5da7267a1c3394e09ec3cc24121d11b836c
Siddheshwar Panda <siddheshwar.2307@gmail.com>
2022-08-14 18:51:18 +0530

    navbar footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Footer from './components/Footer';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <div className="App">
-      Hello
-    </div>
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<>Hello</>} />
+        {/* <Route path='/compose' element={<Compose />} />
+        <Route path='/allmsg' element={<ViewAllMsgs />} /> */}
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
